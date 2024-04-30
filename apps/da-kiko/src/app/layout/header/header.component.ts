db79a533b6c02bb38d83c22cc2d75f25b8357dd9
Greezaaa <greezaaa@gmail.com>
2024-05-01 00:04:46 +0200

    update: Applied translate module oin main and header

--- apps/da-kiko/src/app/layout/header/header.component.ts
+++ apps/da-kiko/src/app/layout/header/header.component.ts
@@ -1,38 +1,39 @@
-import { Component, OnInit } from "@angular/core";
-import { DialogLibrary, DialogMode, SetPreferencesLibrary } from "@lib/shared";
+import { Component, OnInit } from '@angular/core';
+import { DialogLibrary, DialogMode, SetPreferencesLibrary } from '@lib/shared';
+import { TranslateService } from '@ngx-translate/core';
 
 import { languages } from '../../extras/languages';
 @Component({
   standalone: true,
-  selector: "kiko-layout-header",
-  templateUrl: "./header.component.html",
-  imports: [DialogLibrary]
+  selector: 'kiko-layout-header',
+  templateUrl: 'header.component.html',
+  imports: [DialogLibrary],
 })
-export class LayoutHeaderComponent implements OnInit  {
-  
-constructor(private _sP: SetPreferencesLibrary) { }
-  public _langs = languages
-  
-ngOnInit(): void {
-    this.getPreferences()
+export class LayoutHeaderComponent implements OnInit {
+  constructor(private _sP: SetPreferencesLibrary, private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
+  public _langs = languages;
+
+  ngOnInit(): void {
+    this.getPreferences();
   }
 
   state: DialogMode = 0;
   isOpen = false;
-  
+
   getPreferences() {
-   this._sP.getUserSettings() 
+    this._sP.getUserSettings();
   }
 
-  setLang(lang: string): void {
-    this._sP.setUserPreferredLang(lang)
+  switchLang(lang: string) {
+    this.translate.use(lang);
   }
 
   change() {
-    this.state =
-      this.state === DialogMode.login ? DialogMode.signin : DialogMode.login;
+    this.state = this.state === DialogMode.login ? DialogMode.signin : DialogMode.login;
   }
   close() {
     this.isOpen = false;
   }
- } 
+}
--- apps/da-kiko/src/main.ts
+++ apps/da-kiko/src/main.ts
@@ -1,32 +1,45 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom, isDevMode } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { importProvidersFrom, isDevMode, APP_INITIALIZER } from '@angular/core';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { alertReducer, AlertEffects, menuReducer, MenuEffects } from '@lib/shared';
+import { alertReducer, AlertEffects } from '@lib/shared';
 import { provideEffects } from '@ngrx/effects';
+import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app/layout/app/app.component';
 
+// Setup for ngx-translate
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 const reducers = {
-  alerts: alertReducer, 
-  menu: menuReducer, 
+  alerts: alertReducer,
 };
-const effects = [ 
-  AlertEffects, 
-  MenuEffects
-]
+const effects = [AlertEffects];
+
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
     provideEffects(effects),
     provideStore(reducers),
     provideStoreDevtools({
-      maxAge: 25, 
-      logOnly: !isDevMode(), 
-      autoPause: true, 
-      trace: false, 
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
       traceLimit: 75,
     }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 }).catch((err) => console.error(err));
